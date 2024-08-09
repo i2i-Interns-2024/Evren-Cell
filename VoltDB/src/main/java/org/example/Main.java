5182aba4b2384499e9017af02b34b02d9aabfefe
Enes <enestahtaci03@gmail.com>
2024-08-09 11:48:10 +0300

    created new procedures

--- VoltDB/src/main/java/org/example/Main.java
+++ VoltDB/src/main/java/org/example/Main.java
@@ -19,13 +19,13 @@ public class Main {
             // VoltDB sunucusuna bağlanıyoruz
             client.createConnection("34.132.46.242:32803"); //usa server
 
-            ClientResponse response0 = client.callProcedure("PROC1", "2");
+            ClientResponse response0 = client.callProcedure("SHOW_AMOUNT_DATA", "5551234567");
 
             if (response0.getStatus() == ClientResponse.SUCCESS) {
                 VoltTable results = response0.getResults()[0];
                 while (results.advanceRow()) {
                     System.out.println(
-                            "CUST_ID: " + results.getString("PACKAGE_NAME") /*+
+                            "AMOUNT_DATA: " + results.getLong("AMOUNT_DATA") /*+
                                     ", MSISDN: " + results.getString("MSISDN") +
                                     ", NAME: " + results.getString("NAME") +
                                     ", SURNAME: " + results.getString("SURNAME") +
