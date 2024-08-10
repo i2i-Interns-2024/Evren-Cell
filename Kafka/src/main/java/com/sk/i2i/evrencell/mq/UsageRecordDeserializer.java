6b375680a6f1e08d428014b0c71e2020c495d30a
Mustafa Yasin Emir <77809017+myemirrr@users.noreply.github.com>
2024-08-10 17:43:35 +0300

    Delete Kafka directory

--- Kafka/src/main/java/com/sk/i2i/evrencell/mq/UsageRecordDeserializer.java
+++
@@ -1,7 +0,0 @@
-package com.sk.i2i.evrencell.mq;
-
-public class UsageRecordDeserializer extends GenericMessageDeserializer<UsageRecord> {
-    public UsageRecordDeserializer() {
-        super(UsageRecord.class);
-    }
-}
