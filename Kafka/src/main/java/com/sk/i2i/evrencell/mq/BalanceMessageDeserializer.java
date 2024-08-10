c002d838bda861c8b74759b0b008a2ca2188ff8e
Mustafa Yasin Emir <77809017+myemirrr@users.noreply.github.com>
2024-08-10 15:49:56 +0300

    Delete Kafka directory

--- Kafka/src/main/java/com/sk/i2i/evrencell/mq/BalanceMessageDeserializer.java
+++
@@ -1,7 +0,0 @@
-package com.sk.i2i.evrencell.mq;
-
-public class BalanceMessageDeserializer extends GenericMessageDeserializer<BalanceMessage> {
-    public BalanceMessageDeserializer() {
-        super(BalanceMessage.class);
-    }
-}
--- Kafka/src/main/java/com/sk/i2i/evrencell/mq/Main.java
+++
@@ -1,9 +0,0 @@
-package com.sk.i2i.evrencell.mq;
-
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-}
