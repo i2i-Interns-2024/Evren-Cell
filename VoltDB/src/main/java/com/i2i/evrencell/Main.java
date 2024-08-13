57d697992dc80969ae424f748dbe7db95492938b
Enes <enestahtaci03@gmail.com>
2024-08-13 23:34:03 +0300

    ip port change

--- VoltDB/src/main/java/com/i2i/evrencell/Main.java
+++ VoltDB/src/main/java/com/i2i/evrencell/Main.java
@@ -1,41 +1,21 @@
 package com.i2i.evrencell;
 
-import com.i2i.evrencell.voltdb.VoltCustomer;
-import com.i2i.evrencell.voltdb.VoltPackage;
 import com.i2i.evrencell.voltdb.VoltdbOperator;
 import org.voltdb.client.ProcCallException;
-
 import java.io.IOException;
-import java.sql.Timestamp;
-import java.util.Date;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException, ProcCallException {
+    public static void main(String[] args) {
 
         double startTime = System.currentTimeMillis();
         VoltdbOperator voltdbOperator = new VoltdbOperator();
 
-        //voltdbOperator.updatePassword("buBenimYepyeniParolam", "john.doe@example.com","12345678901");
-        //System.out.println(voltdbOperator.getCustomerIdByEmailAndTc("john.doe@example.com","12345678901"));
+        System.out.println(voltdbOperator.getSmsBalance("5551234567"));
+        System.out.println(voltdbOperator.getVoiceBalance("5551234567"));
+        System.out.println(voltdbOperator.getDataBalance("5551234567"));
 
-        //System.out.println(voltdbOperator.checkCustomerExists("john.doe@example.com","12345678901"));
-/*
+        voltdbOperator.close();
 
-        voltdbOperator.updateDataBalance(1500,"5444444486");
-        voltdbOperator.updateSmsBalance(1500,"5444444486");
-        voltdbOperator.updateVoiceBalance(1500,"5444444486");
-*/
-
-        voltdbOperator.updatePassword("john.doe@example.com","12345678901","selam12212");
-        /*voltdbOperator.updateVoiceBalance(10,"haci");
-        voltdbOperator.updateSmsBalance(100,"haci");
-        voltdbOperator.updateDataBalance(100,"haci");*/
-        /*voltdbOperator.updateVoiceBalance(620,"5537030253");
-        voltdbOperator.updateSmsBalance(280,"5537030253");
-*/
-        /*voltdbOperator.insertCustomer(87,"son1","son1","son1","son1","son",new Timestamp(System.currentTimeMillis()),"son1");
-        voltdbOperator.insertBalance(73,87,3,999,999,999,new Timestamp(System.currentTimeMillis()),new Timestamp(System.currentTimeMillis()));
-        System.out.println(voltdbOperator.getPackageInfoByPackageId(2));*/
 
         double endTime = System.currentTimeMillis();
         double elapsedTime = endTime - startTime;
