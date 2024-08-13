d72c5161134dd702fcb2078193df74b820dd111f
Emirhan Kaplan <151027900+emirhankaplan@users.noreply.github.com>
2024-08-13 16:35:56 +0300

    Update Configuration.java

--- Hazelcast/src/main/java/org/i2i/hazelcast/utils/configurations/Configuration.java
+++ Hazelcast/src/main/java/org/i2i/hazelcast/utils/configurations/Configuration.java
@@ -6,6 +6,7 @@ import org.i2i.hazelcast.utils.constants.StringConstants;
 public class Configuration {
     public static ClientConfig getConfig(){
         ClientConfig config = new ClientConfig();
+        config.setProperty("hazelcast.logging.type", "slf4j");
         config.getNetworkConfig().addAddress(StringConstants.hazelcastUrl);
         return config;
     }
