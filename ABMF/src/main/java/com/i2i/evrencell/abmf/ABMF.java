2addd38290a275e9691e9445e3d49ca26900c636
ahmetsefacetin <ahmet80sefa@hotmail.com>
2024-08-13 15:08:55 +0300

    log4j added to ABMF module

--- ABMF/src/main/java/com/i2i/evrencell/abmf/ABMF.java
+++ ABMF/src/main/java/com/i2i/evrencell/abmf/ABMF.java
@@ -4,10 +4,14 @@ import com.i2i.evrencell.kafka.Subscriber;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import com.i2i.evrencell.kafka.message.BalanceMessage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.sql.SQLException;
 
 public class ABMF {
+
+    private static final Logger logger = LogManager.getLogger(ABMF.class);
 
     public static void main(String[] args) throws SQLException {
         String oracleUrl = ConfigLoader.getProperty("oracle.url");
@@ -24,10 +28,19 @@ public class ABMF {
             ConsumerRecords<String, BalanceMessage> records = subscriber.poll();
             for (ConsumerRecord<String, BalanceMessage> record : records) {
                 BalanceMessage message = record.value();
-                System.out.println(message);
+                logBalanceMessage(message);
                 oracleOperations.updateUserBalance(message);
             }
         }
 
+    }
+
+    public static void logBalanceMessage(BalanceMessage message) {
+        String logMessage = String.format("BalanceMessage [MSISDN: %s, Amount: %d, Type: %s]",
+                message.getMsisdn(),
+                message.getAmount(),
+                message.getType());
+
+        logger.info(logMessage);
     }
 }
