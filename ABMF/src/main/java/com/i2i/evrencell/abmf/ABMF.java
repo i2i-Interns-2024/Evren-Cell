02df890c48d24f0e44dc57a9086ed118d5b82773
ahmetsefacetin <ahmet80sefa@hotmail.com>
2024-08-14 17:16:11 +0300

    enough is enough

--- ABMF/src/main/java/com/i2i/evrencell/abmf/ABMF.java
+++ ABMF/src/main/java/com/i2i/evrencell/abmf/ABMF.java
@@ -4,14 +4,10 @@ import com.i2i.evrencell.kafka.Subscriber;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import com.i2i.evrencell.kafka.message.BalanceMessage;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.sql.SQLException;
 
 public class ABMF {
-
-    private static final Logger logger = LogManager.getLogger(ABMF.class);
 
     public static void main(String[] args) throws SQLException {
 
@@ -24,18 +20,8 @@ public class ABMF {
             for (ConsumerRecord<String, BalanceMessage> record : records) {
                 BalanceMessage message = record.value();
                 OracleOperations.updateUserBalance(message);
-                logBalanceMessage(message);
             }
         }
-
     }
 
-    public static void logBalanceMessage(BalanceMessage message) {
-        String logMessage = String.format("BalanceMessage [MSISDN: %s, Amount: %d, Type: %s]",
-                message.getMsisdn(),
-                message.getAmount(),
-                message.getType());
-
-        logger.info(logMessage);
-    }
 }
